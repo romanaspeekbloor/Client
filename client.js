0c8be464a1b35d3e9ea7436fbc9ee4c3fade4306
n3op2 <paulius.michelevicius@gmail.com>
2019-12-14 23:04:21 +0000

    updated client to use socket.io instead of ws.

--- client.js
+++ client.js
@@ -1,76 +1,8 @@
 #!/usr/bin/env node
 // Imports/Dependencies
-const WebSocket = require('ws');
 const io = require('socket.io-client')('http://192.168.10.242:9955');
 const exec = require('child_process').exec;
 const ENV = process.env;
-
-const ws = new WebSocket(ENV.WS_SERVER);
-
-// test
-io.on('connect', () => {
-  console.log('connected');
-  io.emit('myEvent', 'a', 'b', 'c');
-});
-
-// test
-io.on('myEvent', (msg) => console.log(msg));
-
-// test
-io.on('test', (d) => {
-  console.log({ d });
-});
-
-// getSamples event handlers
-io.on('getSamples', async (msg) => {
-  const d = JSON.parse(msg);
-  if (d.actions) {
-    // TODO actions
-    return console.log('actions:' , d.actions, { d });
-  }
-  const unixTS = new Date().getTime();
-  let now = Now();
-  let c = 0n;
-  const offset = unixTS - d.serverTime;
-  const mark = (d.execT - offset) * 1000000;
-  let end = now;;
-
-  while (mark >= c) {
-    if (now > end) {
-      c += now - end;
-      end = now;
-    }
-    now = Now(); 
-  }
-
-  console.log('off: ', offset, 'now: ', now, ' end: ', end, ' st: ', d.serverTime, d.execT, 'c: ', c, '\nmark: ', mark);
-  const execOffset = parseInt(c) - mark;
-  const startedAt = new Date().getTime();
-  const cmd = 'rtl_power -f 153084000:153304000:0.8k -g 35 -i 0 -e -1 2>&1';
-  const raw = await sample(cmd);
-  const { err, out, t} = raw;
-
-  const res = {
-    name: ENV.NAME,
-    msg: 'Sampling done!',
-    execOffset,
-    serverMsgAt: unixTS, 
-    samplingTime: {
-      unit: 'ns',
-      time: t - execOffset
-    },
-    startedAt,
-    data: err ? null : out,
-    error: err ? 'error...' : null,
-    timestamp: new Date().getTime()
-  };
-
-  ws.send(JSON.stringify(res));
-});
-
-ws.on('open', () => {
-  ws.send(`{ENV.NAME} has connected.`);
-});
 
 const convertNs = (t, ns) => {
   const units = ({
@@ -94,16 +26,26 @@ const sample = (cmd) => new Promise(r => {
   });
 });
 
-ws.on('message', async (msg) => {
-  /*
+// Events 
+// =====================================================
+io.on('connect', () => {
+  console.log('connected');
+});
+
+io.on('setClient', (config) => {
+  // TODO client configuration 'init'
+  console.log({ config });
+});
+
+// getSamples event handler
+io.on('getSamples', async (msg) => {
   const d = JSON.parse(msg);
   if (d.actions) {
     // TODO actions
     return console.log('actions:' , d.actions, { d });
   }
   const unixTS = new Date().getTime();
-  let now = Now();
-  let c = 0n;
+  let now = Now(), c = 0n;
   const offset = unixTS - d.serverTime;
   const mark = (d.execT - offset) * 1000000;
   let end = now;;
@@ -138,7 +80,6 @@ ws.on('message', async (msg) => {
     timestamp: new Date().getTime()
   };
 
-  ws.send(JSON.stringify(res));
-  */
+  io.emit('getSamples', JSON.stringify(res));
 });
 
