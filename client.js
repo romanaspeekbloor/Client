cf6be1c04b7ad5cf9244b6e1610d576dd8e53832
n3op2 <paulius.michelevicius@gmail.com>
2019-12-14 22:45:11 +0000

    migrating to socket.io

--- client.js
+++ client.js
@@ -1,38 +1,28 @@
 #!/usr/bin/env node
 // Imports/Dependencies
 const WebSocket = require('ws');
+const io = require('socket.io-client')('http://192.168.10.242:9955');
 const exec = require('child_process').exec;
 const ENV = process.env;
 
 const ws = new WebSocket(ENV.WS_SERVER);
 
-ws.on('open', () => {
-  ws.send(`{ENV.NAME} has connected.`);
+// test
+io.on('connect', () => {
+  console.log('connected');
+  io.emit('myEvent', 'a', 'b', 'c');
 });
 
-const convertNs = (t, ns) => {
-  const units = ({
-    ns: ns,
-    micro: ns / 1000,
-    ms: ns / 1000000,
-  });
-  // TODO check if possible to convert to max int
-  return parseInt(units[t]);
-};
+// test
+io.on('myEvent', (msg) => console.log(msg));
 
-const Now = () => process.hrtime.bigint();
-
-const sample = (cmd) => new Promise(r => {
-  const start = Now();
-  console.log('scanning...');
-  exec(cmd, (err, out) => {
-    const t = parseInt(Now() - start);
-    if (err) return r({ err, out, t });
-    r({ err, out, t }); 
-  });
+// test
+io.on('test', (d) => {
+  console.log({ d });
 });
 
-ws.on('message', async (msg) => {
+// getSamples event handlers
+io.on('getSamples', async (msg) => {
   const d = JSON.parse(msg);
   if (d.actions) {
     // TODO actions
@@ -64,7 +54,7 @@ ws.on('message', async (msg) => {
     name: ENV.NAME,
     msg: 'Sampling done!',
     execOffset,
-    startTime: unixTS, 
+    serverMsgAt: unixTS, 
     samplingTime: {
       unit: 'ns',
       time: t - execOffset
@@ -76,5 +66,79 @@ ws.on('message', async (msg) => {
   };
 
   ws.send(JSON.stringify(res));
+});
+
+ws.on('open', () => {
+  ws.send(`{ENV.NAME} has connected.`);
+});
+
+const convertNs = (t, ns) => {
+  const units = ({
+    ns: ns,
+    micro: ns / 1000,
+    ms: ns / 1000000,
+  });
+  // TODO check if possible to convert to max int
+  return parseInt(units[t]);
+};
+
+const Now = () => process.hrtime.bigint();
+
+const sample = (cmd) => new Promise(r => {
+  const start = Now();
+  console.log('scanning...');
+  exec(cmd, (err, out) => {
+    const t = parseInt(Now() - start);
+    if (err) return r({ err, out, t });
+    r({ err, out, t }); 
+  });
+});
+
+ws.on('message', async (msg) => {
+  /*
+  const d = JSON.parse(msg);
+  if (d.actions) {
+    // TODO actions
+    return console.log('actions:' , d.actions, { d });
+  }
+  const unixTS = new Date().getTime();
+  let now = Now();
+  let c = 0n;
+  const offset = unixTS - d.serverTime;
+  const mark = (d.execT - offset) * 1000000;
+  let end = now;;
+
+  while (mark >= c) {
+    if (now > end) {
+      c += now - end;
+      end = now;
+    }
+    now = Now(); 
+  }
+
+  console.log('off: ', offset, 'now: ', now, ' end: ', end, ' st: ', d.serverTime, d.execT, 'c: ', c, '\nmark: ', mark);
+  const execOffset = parseInt(c) - mark;
+  const startedAt = new Date().getTime();
+  const cmd = 'rtl_power -f 153084000:153304000:0.8k -g 35 -i 0 -e -1 2>&1';
+  const raw = await sample(cmd);
+  const { err, out, t} = raw;
+
+  const res = {
+    name: ENV.NAME,
+    msg: 'Sampling done!',
+    execOffset,
+    serverMsgAt: unixTS, 
+    samplingTime: {
+      unit: 'ns',
+      time: t - execOffset
+    },
+    startedAt,
+    data: err ? null : out,
+    error: err ? 'error...' : null,
+    timestamp: new Date().getTime()
+  };
+
+  ws.send(JSON.stringify(res));
+  */
 });
 
